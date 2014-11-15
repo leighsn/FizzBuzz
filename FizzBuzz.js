da521bd44e7f612c951e2923eb9d4705ed74c238
leighsn <leighscherrer@gmail.com>
2014-11-15 14:10:58 -0500

    Initial commit

---
+++ FizzBuzz.js
@@ -0,0 +1,16 @@
+for(count=0;count<100;count++)
+{
+	if(count%15==0)
+ 	{
+   		console.log("FizzBuzz");
+    }
+   	else if(count%3==0)
+    {
+     	console.log("Fizz"); 
+    }
+    else if ( count % 5==0){
+      	console.log("Buzz");
+    }
+    else{
+      console.log(count);
+  }
